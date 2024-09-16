ed2350c3fff8b9b7bf935270bf028cdbcc959639
ZhouShichan <zsc19823382069@163.com>
2024-09-16 15:20:44 +0800

    feat: 实现基于图卷积网络(GCN)的欺诈用户识别模型

---
+++ main_GCN.py
@@ -0,0 +1,64 @@
+from tqdm import tqdm
+import os
+from pathlib import Path
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn.functional as F
+from torch import Tensor
+from torch.utils.data import DataLoader
+
+from models.GCN import Model
+from utils.dgraphfin import load_data, AdjacentNodesDataset
+from utils.evaluator import Evaluator
+
+# 设置gpu设备
+device = 0
+device = f'cuda:{device}' if torch.cuda.is_available() else 'cpu'
+device = torch.device(device)
+
+data = load_data('./datasets/632d74d4e2843a53167ee9a1-momodel/', 'DGraph', force_to_symmetric=True)
+data = data.to(device)
+
+model_params = {
+    "h_c": 16,
+    "n_layers": 2,
+    "dropout": 0.1,
+    "normalize": True,
+}
+
+model = Model(
+    in_c=20,
+    out_c=2,
+    ** model_params
+).to(device)
+model_desc = f'GCN-{"-".join([f"{k}_{v}" for k, v in model_params.items() ])}'
+model_save_path = f'results/model-{model_desc}.pt'
+model.load_state_dict(torch.load(model_save_path, map_location=device))
+
+cache_path = f'./results/out-{model_desc}.pt'
+
+
+def predict(data, node_id):
+    if os.path.exists(cache_path):
+        out = torch.load(cache_path)
+    else:
+        with torch.no_grad():
+            model.eval()
+            out = model(data.x, data.edge_index)
+
+    pred = out[node_id].exp()
+    return pred.squeeze(0)
+
+
+if __name__ == '__main__':
+    dic = {0: "正常用户", 1: "欺诈用户"}
+    node_idx = 0
+    y_pred = predict(data, node_idx)
+    print(y_pred)
+    print(f'节点 {node_idx} 预测对应的标签为:{torch.argmax(y_pred)}, 为{dic[torch.argmax(y_pred).item()]}。')
+
+    node_idx = 1
+    y_pred = predict(data, node_idx)
+    print(y_pred)
+    print(f'节点 {node_idx} 预测对应的标签为:{torch.argmax(y_pred)}, 为{dic[torch.argmax(y_pred).item()]}。')
---
+++ models/GCN.py
@@ -0,0 +1,41 @@
+import torch
+import torch.nn.functional as F
+
+from torch_geometric.nn import GCNConv, GATConv
+
+
+class Model(torch.nn.Module):
+    def __init__(
+            self,
+            in_c: int,
+            h_c: int,
+            out_c: int,
+            n_layers: int = 2,
+            dropout: float = 0.1,
+            normalize: bool = True,
+    ):
+        super(Model, self).__init__()
+        self.n_layers = n_layers
+
+        self.convs = torch.nn.ModuleList([
+            GCNConv(
+                in_c if i == 0 else h_c,
+                h_c if i != n_layers - 1 else out_c,
+                normalize=normalize,
+                cached=True
+            )
+            for i in range(n_layers)
+        ])
+        self.dropout = dropout
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+
+    def forward(self, x, edge_index, edge_weight=None, **kwargs):
+        for i in range(self.n_layers - 1):
+            x = self.convs[i](x, edge_index, edge_weight=edge_weight)
+            x = F.relu(x)
+            x = F.dropout(x, p=self.dropout, training=self.training)
+        x = self.convs[-1](x, edge_index)
+        return F.log_softmax(x, dim=-1)
