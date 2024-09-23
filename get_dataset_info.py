ff0fa9c6ff66608c1d7544f6a6033f7202aa0572
ZhouShichan <zsc19823382069@163.com>
2024-09-23 22:07:02 +0800

    feat: 添加了数据集信息获取功能并保存至CSV文件

--- get_dataset_info.py
+++ get_dataset_info.py
@@ -1,10 +1,15 @@
 from tqdm import tqdm
+from pathlib import Path
+import pandas as pd
 
 from utils.dgraphfin import AdjacentNodesDataset, load_data
 
 data = load_data('./datasets/632d74d4e2843a53167ee9a1-momodel/', 'DGraph', force_to_symmetric=True)
 
 datasets = {'train': lambda x: x.train_mask, 'val': lambda x: x.valid_mask, 'test': lambda x: x.test_mask}
+
+result_filepath = Path('./results/dataset_info.csv')
+result_filepath.parent.mkdir(exist_ok=True, parents=True)
 
 for name, mask in datasets.items():
     dataset = AdjacentNodesDataset(data, mask(data), 24)
@@ -20,6 +25,7 @@ for name, mask in datasets.items():
     n_neighbors = dict(sorted(n_neighbors.items(), key=lambda x: x[1], reverse=True))
     too_less = []
     sum_ratio = 0
+    df = {'dataset': [], 'nodes': [], 'ratio': []}
     for k, v in n_neighbors.items():
         ratio = v / len(dataset) * 100.0
         sum_ratio += ratio
@@ -27,4 +33,9 @@ for name, mask in datasets.items():
             too_less.append(k)
         else:
             print(f'nodes {k}\t : \t{v} \t {ratio:.2f}%')
+        df['dataset'].append(name)
+        df['nodes'].append(k)
+        df['ratio'].append(ratio)
+    with result_filepath.open('a', newline='') as f:
+        pd.DataFrame(df).to_csv(f, header=False if f.tell() else True, index=False)
     print(f'Nodes with less than 1% neighbors: {too_less}')
